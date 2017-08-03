4f23c1c3235ce0695b28513d023ad44062a9a061
Kenia Valladarez <keniavalladarez@gmail.com>
2017-08-03 13:04:53 -0400

    [master]  Tests for authenticated user making threads.

---
+++ tests/Feature/CreateThreadsTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class CreateThreadsTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /** @test */
+    function testGuestsMayNotCreateThreads()
+    {
+        $this->expectException('Illuminate\Auth\AuthenticationException');
+        $thread = make('App\Thread');
+        $this->post('/threads', $thread->toArray());
+    }
+
+    /** @test */
+    function testAnAuthenticatedUserCanCreateNewForumThreads()
+    {
+        $this->signIn();
+
+        $thread = make('App\Thread');
+
+        $this->post('/threads', $thread->toArray());
+
+        $this->get($thread->path())
+            ->assertSee($thread->title)
+            ->assertSee($thread->body);
+    }
+}
